4128c277bbc50cd81bfb2300843ba97b023d2b87
Mohit Anand <mohit@Mohits-MacBook-Pro.local>
2020-06-09 15:51:05 +0530

    requirements.txt updated

--- test_script_1.py
+++ test_script_1.py
@@ -9,6 +9,6 @@ fname = "1514765764.ndjson.gz"
 
 path = root_path + data_path
 
-loc_lat_lon = loc_lat_lon(path,2018, "pm10")
+loc_lat_lon = loc_lat_lon(path, 2018, "pm10")
 
 ParameterMaps(loc_lat_lon)
