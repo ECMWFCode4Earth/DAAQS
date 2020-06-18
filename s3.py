76a78810c0db56835de311da0d046a80ad55eab9
Mohit Anand <mohit@Mohits-MacBook-Pro.local>
2020-06-18 08:41:03 +0530

    s3.py

--- s3.py
+++ s3.py
@@ -1,9 +1,11 @@
 import boto3
+from boto3 import s3
 # connect to our S3 endpoint
 resource = boto3.resource('s3',
 endpoint_url = 'https://storage.ecmwf.europeanweather.cloud',
 aws_access_key_id='X9G47J53UC3QAAYWZ1OZ',
 aws_secret_access_key='8a2Yt6wuxdAKLo6erLIws3ni9f0xQ83de1jNxwap')
+
 # connect to S3 bucket
 bucket = resource.Bucket('openaq')
 # list all the objects
